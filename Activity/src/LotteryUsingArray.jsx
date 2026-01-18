099aa0cbce47a709b327ca6f9f73b64d4ceffd22
OmkarD09 <omkardhakane09@gmail.com>
2026-01-18 20:57:00 +0530

    Lottery

--- Activity/src/LotteryUsingArray.jsx
+++ Activity/src/LotteryUsingArray.jsx
@@ -1,6 +1,34 @@
 import { useState } from "react";
+import './lottery.css';
+import { genTicket, sumOfArray } from "./helper";
+
 
 
 function LotteryUsingArray() {
-    const [lotteryNumbers, setLotteryNumbers] = useState([]);
+    let [tickets, setTickets] = useState(genTicket(3));
+
+    let isWinning = sumOfArray(tickets) === 15;
+
+
+
+
+
+    return (
+        <div>
+            <h2>Lottery Using Array</h2>
+            <div className="tickets">
+                <span>{tickets[0]}</span>
+                <span>{tickets[1]}</span>
+                <span>{tickets[2]}</span>
+            </div>
+
+            <br />
+
+            <button onClick={() => setTickets(genTicket(3))}>Get New Ticket</button>
+
+            <div>{isWinning ? "You Win!" : "Try Again"}</div>
+        </div>
+    );
 }
+
+export default LotteryUsingArray;
